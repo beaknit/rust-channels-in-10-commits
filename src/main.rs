2b1acc2b2a8416e648f158fb74145e8200ec6a44
Nathan McCourtney <nathan@mccourtney.com>
2021-11-18 16:42:19 -0500

    Broken generic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use tokio::sync::mpsc;
 
 #[tokio::main]
@@ -17,15 +18,19 @@ async fn route(tx: mpsc::Sender<Message>) {
 async fn biz_logix(tx: mpsc::Sender<Message>) {
     for x in 1..=50 {
         println!("Call number {}", x);
-        let tx_clone = tx.clone();
-        let msg = Message {
+        let msg1 = Message { value: x };
+        send_msg(&tx, msg1);
+
+        let msg2 = Message {
             value: format!("Call number {} done!!", x),
         };
-        tx_clone.send(msg).await.unwrap();
-
-        let msg2 = Message { value: 2 };
-        tx_clone.send(msg2).await.unwrap()
+        send_msg(&tx, msg2);
     }
+}
+
+async fn send_msg(tx: &mpsc::Sender<Message>, msg: Message) {
+    let tx_clone = tx.clone();
+    tx_clone.send(msg).await.unwrap();
 }
 
 async fn call_service(msg: &str) {
@@ -47,6 +52,6 @@ async fn run_client_pool(mut receiver: mpsc::Receiver<Message>) {
 }
 
 #[derive(Debug)]
-struct Message {
-    value: String,
+struct Message<T> {
+    value: T,
 }
